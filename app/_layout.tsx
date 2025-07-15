70b5f778a47ef57b0f6bd8fb510ae9fb2091aa0c
Mahmoud-S97 <mhmodhr@gmail.com>
2025-07-15 19:12:42 +0100

    First Commit, uploading files

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import "./globals.css";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
